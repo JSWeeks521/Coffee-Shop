4e35a2408df1e7f54dd40a3ec3f9b358d6d4962d
JSWeeks <paintingwithjason@gmail.com>
2020-12-31 12:45:56 -0500

    Added AuthO credentials

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,6 +2,20 @@
  * ensure all variables on this page match your project
  */
 
+
+ export const environment = {
+   production: false,
+   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+   auth0: {
+     url: 'dev-2o9neg10.us', // the auth0 domain prefix
+     audience: 'UdacityCoffeeShop', // the audience set for the auth0 app
+     clientId: 'LIMwMmH2aJ5bg0EXUcfH5rBquAmIOLkc', // the client id generated for the auth0 app
+     callbackURL: 'http://127.0.0.1:8100/tabs/user-page', // the base url of the running ionic application.
+   }
+ };
+
+
+/*
 export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
@@ -9,7 +23,7 @@ export const environment = {
     url: '', // the auth0 domain prefix
     audience: '', // the audience set for the auth0 app
     clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
   }
 };
-
+ */
